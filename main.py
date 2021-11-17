fc37844b898ed42fae8b35da9c154cde5e7d7af4
shuaizhenli <li.shuai@husky.neu.edu>
2021-11-17 13:41:25 -0800

    added flask for webservices

--- main.py
+++ main.py
@@ -1,15 +1,164 @@
 import sys
 import argparse
-from process import check_run
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("pathToStudentsFile")
-    parser.add_argument("pathToMarksFile")
-    parser.add_argument("pathToTestsFile")
-    parser.add_argument("pathToCoursesFile")
-    parser.add_argument("pathToOutputFile")
-    args = parser.parse_args()
+from process import check_run_api
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
+import pandas as pd
+import json
 
-    check_run(args.pathToStudentsFile, args.pathToMarksFile, args.pathToTestsFile, args.pathToCoursesFile, args.pathToOutputFile)
+app = Flask(__name__)
+api = Api(app)
 
-main()
+class Records(Resource):
+    
+    def get(self):
+        return check_run_api("students.csv", "marks.csv", "tests.csv", "courses.csv")
+api.add_resource(Records, '/records')
+
+
+
+
+class Students(Resource):
+    def __init__(self):
+        self.data = pd.read_csv("students.csv")
+        self.js_data = self.data.to_json(orient="records")
+
+    def get(self):
+        return {'data': json.loads(self.js_data)}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        
+        parser.add_argument('id', required=True)  # add args
+        parser.add_argument('name', required=True)
+                
+        args = parser.parse_args()
+        data = pd.read_csv('students.csv')
+        if int(args['id']) in list(data['id']):
+            return {
+                'message': f"'{args['id']}' already exists in students table."
+            }, 401
+        elif not args['name']:
+            return {
+                'message': f" Student name: '{args['name']}' contains invalid value."
+            }, 401
+        else:
+            new_data = pd.DataFrame({
+                'id': int(args['id']),
+                'name': args['name'],
+            }, index = [0])
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('students.csv', index=False)
+            return {'data': json.loads(self.js_data)}, 200
+
+
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('id', required=True)  # add args
+        parser.add_argument('name', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read CSV
+        data = pd.read_csv('students.csv')
+
+        if int(args['id']) in list(data['id']):
+            data['name'][data['id'] == int(args['id'])] = args['name']
+
+        # save back to CSV
+            data.to_csv('students.csv', index=False)
+            # return data and 200 OK
+            read = pd.read_csv("students.csv")
+            js_data_put = read.to_json(orient="records")
+            return {'data': json.loads(js_data_put)}, 200
+        else:
+            # otherwise the student does not exist
+            return {
+                'message': f"'{args['id']}' student not found."
+            }, 404
+
+
+    def delete(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('id', required=True)  # add args
+        args = parser.parse_args()  # parse arguments to dictionary
+        data = pd.read_csv('students.csv')
+
+        if int(args['id']) in list(data['id']):
+            data = data[data.id != int(args['id'])]
+
+        # save back to CSV
+            data.to_csv('students.csv', index=False)
+            # return data and 200 OK
+            read = pd.read_csv("students.csv")
+            js_data_put = read.to_json(orient="records")
+            return {'data': json.loads(js_data_put)}, 200
+        else:
+            # otherwise the userId does not exist
+            return {
+                'message': f"'{args['id']}' student not found."
+            }, 404
+
+api.add_resource(Students, '/students')
+
+
+
+
+class Marks(Resource):
+    def __init__(self):
+        self.data = pd.read_csv("marks.csv")
+        self.js_data = self.data.to_json(orient="records")
+    def get(self):
+        return {'data': json.loads(self.js_data)}, 200
+api.add_resource(Marks, '/marks')
+
+
+
+class Tests(Resource):
+    def __init__(self):
+        self.data = pd.read_csv("tests.csv")
+        self.js_data = self.data.to_json(orient="records")
+    def get(self):
+        return {'data': json.loads(self.js_data)}, 200         
+api.add_resource(Tests, '/tests')
+
+
+
+
+class Courses(Resource):
+    def __init__(self):
+        self.data = pd.read_csv("courses.csv")
+        self.js_data = self.data.to_json(orient="records")
+    def get(self):
+        return {'data': json.loads(self.js_data)}, 200
+api.add_resource(Courses, '/courses')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("pathToStudentsFile")
+#     parser.add_argument("pathToMarksFile")
+#     parser.add_argument("pathToTestsFile")
+#     parser.add_argument("pathToCoursesFile")
+#     parser.add_argument("pathToOutputFile")
+#     args = parser.parse_args()
+
+#     check_run(
+#         args.pathToStudentsFile,
+#         args.pathToMarksFile,
+#         args.pathToTestsFile,
+#         args.pathToCoursesFile,
+#         args.pathToOutputFile,
+#     )
+
+
+# check_run('students.csv', 'marks.csv', 'tests.csv', 'courses.csv', 'result')
+# main()
--- process.py
+++ process.py
@@ -3,41 +3,49 @@ import pandas as pd
 import numpy as np
 import json
 import os
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
 
 def checkTests(testsCSV):
     result = "pass"
     df = pd.read_csv(testsCSV)
     if df.isnull().values.any():
         result = "null"
-    group = df.groupby('course_id')
-    groupDf = [v for k, v in df.groupby('course_id')]
-    if df['id'].min() < 0:
+    group = df.groupby("course_id")
+    groupDf = [v for k, v in df.groupby("course_id")]
+    if df["id"].min() < 0:
         result = "id"
     for i in groupDf:
-        if i['weight'].sum() != 100:
+        if i["weight"].sum() != 100:
             result = "weight"
-        
+
     return result
+
 
 def checkStudents(studentsCSV):
     result = "pass"
     df = pd.read_csv(studentsCSV)
     if df.isnull().values.any():
         result = "null"
-    if df['id'].min() < 0:
+    if df["id"].min() < 0:
         result = "id"
     return result
-#print(checkStudents('students.csv'))
 
-    
+
+# print(checkStudents('students.csv'))
+
+
 def checkCourses(coursesCSV):
     result = "pass"
     df = pd.read_csv(coursesCSV)
     if df.isnull().values.any():
         result = "null"
-    if df['id'].min() < 0:
+    if df["id"].min() < 0:
         result = "id"
     return result
+
+
 def checkMarks(marksCSV, studentsCSV, testsCSV):
     result = "pass"
     marksDf = pd.read_csv(marksCSV)
@@ -45,29 +53,31 @@ def checkMarks(marksCSV, studentsCSV, testsCSV):
     testsDf = pd.read_csv(testsCSV)
     if marksDf.isnull().values.any():
         result = "null"
-    for i in marksDf['test_id'].unique():
-        if i not in testsDf['id'].unique():
+    for i in marksDf["test_id"].unique():
+        if i not in testsDf["id"].unique():
             result = "notInTest"
-    for i in marksDf['student_id'].unique():
-        if i not in studentsDf['id'].unique():
+    for i in marksDf["student_id"].unique():
+        if i not in studentsDf["id"].unique():
             result = "notInStu"
     return result
-#print(checkMarks('marks.csv','students.csv','tests.csv'))
 
 
-#[{'id': 1, 'name': 'A', 'totalAverage': None, 'courses': []}, 
-# {'id': 2, 'name': 'B', 'totalAverage': None, 'courses': []}, 
+# print(checkMarks('marks.csv','students.csv','tests.csv'))
+
+
+# [{'id': 1, 'name': 'A', 'totalAverage': None, 'courses': []},
+# {'id': 2, 'name': 'B', 'totalAverage': None, 'courses': []},
 # {'id': 3, 'name': 'C', 'totalAverage': None, 'courses': []}]
 def constructJsonValue(studentCSV):
     students = []
     for row in studentCSV:
-        temp_student_index = row['id']
-        row['id'] = int(temp_student_index)
+        temp_student_index = row["id"]
+        row["id"] = int(temp_student_index)
         students.append(row)
 
     for i in students:
-        i['totalAverage'] = None
-        i['courses'] = []
+        i["totalAverage"] = None
+        i["courses"] = []
     return students
 
 
@@ -78,13 +88,13 @@ def constructJsonValue(studentCSV):
 # 3           1        5    65          2      60  History   Mrs. P
 # 4           1        3    95          1      50  Biology    Mr. D
 # 5           1        4    32          2      40  History   Mrs. P
-# 6           1        2    87          1      40  Biology    Mr. D,     
+# 6           1        2    87          1      40  Biology    Mr. D,
 #      student_id  test_id  mark  course_id  weight     name  teacher
 # 7            2        2    87          1      40  Biology    Mr. D
 # 8            2        3    15          1      50  Biology    Mr. D
 # 9            2        7    40          3      10     Math   Mrs. C
 # 10           2        6    78          3      90     Math   Mrs. C
-# 11           2        1    78          1      10  Biology    Mr. D,     
+# 11           2        1    78          1      10  Biology    Mr. D,
 #      student_id  test_id  mark  course_id  weight     name  teacher
 # 12           3        3    95          1      50  Biology    Mr. D
 # 13           3        4    32          2      40  History   Mrs. P
@@ -97,15 +107,14 @@ def constructIndividualCourseList(marksCSV, testsCSV, coursesCSV):
     marks_df = pd.read_csv(marksCSV)
     tests_df = pd.read_csv(testsCSV)
     courses_df = pd.read_csv(coursesCSV)
-    tests_df = tests_df.rename(columns={'id': 'test_id'})
-    courses_df = courses_df.rename(columns={'id': 'course_id'})
-    #marks_df["course_id"] = marks_df["test_id"].map(tests_df["id"])
-    df = pd.merge(marks_df, tests_df, on='test_id')
-    df = pd.merge(df, courses_df, on='course_id')
-    student_id_col = df.pop('student_id')
-    df.insert(0, 'student_id', student_id_col)
-    df = df.sort_values(['student_id']).reset_index(drop=True)
-    student_record_list = [v for k, v in df.groupby('student_id')]
+    tests_df = tests_df.rename(columns={"id": "test_id"})
+    courses_df = courses_df.rename(columns={"id": "course_id"})
+    df = pd.merge(marks_df, tests_df, on="test_id")
+    df = pd.merge(df, courses_df, on="course_id")
+    student_id_col = df.pop("student_id")
+    df.insert(0, "student_id", student_id_col)
+    df = df.sort_values(["student_id"]).reset_index(drop=True)
+    student_record_list = [v for k, v in df.groupby("student_id")]
     return student_record_list
 
 
@@ -127,67 +136,93 @@ def w_avg(df, values, weights):
 # 1   2  History   Mrs. P           51.8           3
 # 2   3     Math   Mrs. C           74.2           3
 def createCourseRecord(student_record_df):
-    sorted_by_course = student_record_df.sort_values(['course_id']).reset_index(drop=True)
-    df = sorted_by_course.groupby('course_id').apply(w_avg, 'mark', 'weight')
-    df = pd.merge(df.to_frame(), sorted_by_course, on='course_id')
-    essential_info = df.drop(['mark', 'weight', 'test_id'], axis = 1).drop_duplicates().reset_index(drop=True)
-    teacher_name_col = essential_info.pop('teacher')
-    essential_info.insert(0, 'teacher', teacher_name_col)
-    course_name_col = essential_info.pop('name')
-    essential_info.insert(0, 'name', course_name_col)
-    essential_info = essential_info.rename(columns={0: 'courseAverage'})
-    essential_info = essential_info.rename(columns={'course_id': 'id'})
-    course_id_col = essential_info.pop('id')
-    essential_info.insert(0, 'id', course_id_col)
+    sorted_by_course = student_record_df.sort_values(["course_id"]).reset_index(
+        drop=True
+    )
+    df = sorted_by_course.groupby("course_id").apply(w_avg, "mark", "weight")
+    df = pd.merge(df.to_frame(), sorted_by_course, on="course_id")
+    essential_info = (
+        df.drop(["mark", "weight", "test_id"], axis=1)
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+    teacher_name_col = essential_info.pop("teacher")
+    essential_info.insert(0, "teacher", teacher_name_col)
+    course_name_col = essential_info.pop("name")
+    essential_info.insert(0, "name", course_name_col)
+    essential_info = essential_info.rename(columns={0: "courseAverage"})
+    essential_info = essential_info.rename(columns={"course_id": "id"})
+    course_id_col = essential_info.pop("id")
+    essential_info.insert(0, "id", course_id_col)
     return essential_info
 
 
+def dumpJson(
+    students_filename, marks_filename, tests_filename, courses_filename, output_path
+):
 
-def dumpJson(students_filename,marks_filename, tests_filename, courses_filename, output_path):
-
-    studentCSV = csv.DictReader(open(students_filename))      
+    studentCSV = csv.DictReader(open(students_filename))
     wholeList = constructJsonValue(studentCSV)
 
-    individualCourseList = constructIndividualCourseList(marks_filename, tests_filename, courses_filename)
+    individualCourseList = constructIndividualCourseList(
+        marks_filename, tests_filename, courses_filename
+    )
     studen_courses_dict = {}
-    
+
     for individualCourse in individualCourseList:
         df = createCourseRecord(individualCourse)
-        studen_courses_dict[(df['student_id'].unique()[0])] = df.drop(['student_id'], axis=1)
+        studen_courses_dict[(df["student_id"].unique()[0])] = df.drop(
+            ["student_id"], axis=1
+        )
 
     for student in wholeList:
-        jsdf = studen_courses_dict[int(student['id'])].to_json(orient = 'records')
-        student['courses'] = json.loads(jsdf)
-        student['totalAverage'] = round((studen_courses_dict[int(student['id'])]['courseAverage']).mean(), 2)
+        jsdf = studen_courses_dict[int(student["id"])].to_json(orient="records")
+        student["courses"] = json.loads(jsdf)
+        student["totalAverage"] = round(
+            (studen_courses_dict[int(student["id"])]["courseAverage"]).mean(), 2
+        )
 
     data = {}
-    data['students'] = wholeList
-    
-    filename = './' + output_path + '/result.json'
+    data["students"] = wholeList
+
+    filename = "./" + output_path + "/result.json"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as fp:
+    with open(filename, "w") as fp:
         json.dump(data, fp, indent=2)
 
-def check_run(students_filename,marks_filename, tests_filename, courses_filename ,output_path):
+
+def check_run(
+    students_filename, marks_filename, tests_filename, courses_filename, output_path
+):
     result = ""
     p = "pass"
-    if checkCourses(courses_filename )== p and checkStudents(students_filename) == p and checkTests(tests_filename) == p and checkMarks(marks_filename, students_filename, tests_filename )== p :
+    if (
+        checkCourses(courses_filename) == p
+        and checkStudents(students_filename) == p
+        and checkTests(tests_filename) == p
+        and checkMarks(marks_filename, students_filename, tests_filename) == p
+    ):
         print("all check on JSON files passed.")
-        dumpJson(students_filename,marks_filename, tests_filename, courses_filename, output_path)
-        print("Result JSON file has been created. Please check root directory.")
+        dumpJson(
+            students_filename,
+            marks_filename,
+            tests_filename,
+            courses_filename,
+            output_path,
+        )
+        print("Result JSON file has been created. Please check output directory.")
     else:
         t = checkTests(tests_filename)
         c = checkCourses(courses_filename)
         s = checkStudents(students_filename)
         m = checkMarks(marks_filename, students_filename, tests_filename)
-        #print(t,c,s,m)
+        # print(t,c,s,m)
         if m == "null":
             result = "marks CSV file contains null values"
         if m == "notInTest":
             result = "marksCSV error, Cannot find test ID in test CSV."
         if m == "notInStu":
             result = "marksCSV error, Cannot find student ID in students CSV."
-
 
         if t == "null":
             result = "tests CSV file contains null values"
@@ -206,14 +241,99 @@ def check_run(students_filename,marks_filename, tests_filename, courses_filename
         if s == "id":
             result = "students CSV file contains invalid ids"
 
+    if result:
+        d = {}
+        d["error"] = result
+        filename = "./" + output_path + "/result.json"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as fp:
+            json.dump(d, fp, indent=2)
+
+
+# check_run("students.csv", "marks.csv", "tests.csv","courses.csv")
+# check_run("students.csv", "marks.csv", "tests.csv","courses.csv", "output")
+def dump_json_api(
+    students_filename, marks_filename, tests_filename, courses_filename
+):
+
+    studentCSV = csv.DictReader(open(students_filename))
+    wholeList = constructJsonValue(studentCSV)
+
+    individualCourseList = constructIndividualCourseList(
+        marks_filename, tests_filename, courses_filename
+    )
+    studen_courses_dict = {}
+
+    for individualCourse in individualCourseList:
+        df = createCourseRecord(individualCourse)
+        studen_courses_dict[(df["student_id"].unique()[0])] = df.drop(
+            ["student_id"], axis=1
+        )
+    print(studen_courses_dict)
+    for student in wholeList:
+        if int(student["id"]) in studen_courses_dict:
+            jsdf = studen_courses_dict[int(student["id"])].to_json(orient="records")
+            student["courses"] = json.loads(jsdf)
+            student["totalAverage"] = round(
+                (studen_courses_dict[int(student["id"])]["courseAverage"]).mean(), 2
+            )
+
+    data = {}
+    data["students"] = wholeList
+
+    return data
+
+
+
+def check_run_api(
+    students_filename, marks_filename, tests_filename, courses_filename
+):
+    result = ""
+    p = "pass"
+    if (
+        checkCourses(courses_filename) == p
+        and checkStudents(students_filename) == p
+        and checkTests(tests_filename) == p
+        and checkMarks(marks_filename, students_filename, tests_filename) == p
+    ):
+        print("all check on JSON files passed.")
+        return dump_json_api(
+            students_filename,
+            marks_filename,
+            tests_filename,
+            courses_filename,
+        )
+    else:
+        t = checkTests(tests_filename)
+        c = checkCourses(courses_filename)
+        s = checkStudents(students_filename)
+        m = checkMarks(marks_filename, students_filename, tests_filename)
+        # print(t,c,s,m)
+        if m == "null":
+            result = "marks CSV file contains null values"
+        if m == "notInTest":
+            result = "marksCSV error, Cannot find test ID in test CSV."
+        if m == "notInStu":
+            result = "marksCSV error, Cannot find student ID in students CSV."
+
+        if t == "null":
+            result = "tests CSV file contains null values"
+        if t == "id":
+            result = "tests CSV file contains invalid ids"
+        if t == "weight":
+            result = "Invalid test weights"
+
+        if c == "null":
+            result = "courses CSV file contains null values"
+        if c == "id":
+            result = "courses CSV file contains invalid ids"
+
+        if s == "null":
+            result = "students CSV file contains null values"
+        if s == "id":
+            result = "students CSV file contains invalid ids"
 
     if result:
         d = {}
-        d['error'] = result
-        filename = './' + output_path + '/result.json'
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, 'w') as fp:
-            json.dump(d, fp, indent=2)
-
-#check_run("students.csv", "marks.csv", "tests.csv","courses.csv")
-#check_run("students.csv", "marks.csv", "tests.csv","courses.csv", "output")
+        d["error"] = result
+        return d
